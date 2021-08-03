fac7bd5772e5985f248bd0e3729c0cfa6293969a
RedAFD <radixholms@gmail.com>
2021-08-03 11:26:46 +0800

    update algorithm

--- uratrie.go
+++ uratrie.go
@@ -4,116 +4,188 @@ import (
 	"bytes"
 	"fmt"
 	"regexp"
+	"sort"
 	"strings"
+	"sync"
 
-	"github.com/RoaringBitmap/roaring/roaring64"
+	"github.com/RoaringBitmap/roaring"
 )
 
-type Node struct {
-	Regexps   map[string]*regexp.Regexp
-	Childrens map[string]*Node // segment => node
-	Container *roaring64.Bitmap
-	Labels    map[string]string
-	End       bool
+var bufPool = sync.Pool{
+	New: func() interface{} {
+		return &bytes.Buffer{}
+	},
 }
 
-func (n *Node) ipop(path *[]byte) (seg, label string, w bool) {
+type Node struct {
+	// segment => *regexp.Regexp
+	Regexps map[string]*regexp.Regexp
+	// segment => *Node
+	Childrens map[string]*Node
+	// userIDs
+	Container *roaring.Bitmap
+	// because it is a different branch, it can only be all single or all multiple.
+	// segment index => wildcard key => path key
+	Wildcards map[int]map[string]string
+	// whether leaf node
+	Leaf bool
+}
+
+func (n *Node) ipop(
+	path *[]byte,
+) (
+	segment string,
+	wildcard string,
+	fuzzy bool,
+) {
 	if len(*path) == 0 {
 		return
 	}
-	s := 0
-	xlab := false
-	lbuf := &bytes.Buffer{}
+	s, c, bs, be := 0, 0, -1, -1
+	b := bufPool.New().(*bytes.Buffer)
+	wb := bufPool.New().(*bytes.Buffer)
 	for i := 0; i < len(*path); i++ {
 		switch (*path)[i] {
 		case 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z':
 			(*path)[i] += 32 // lower case
+		case '{':
+			bs = i
+		case '}':
+			be = i
 		case '/':
-			seg = string((*path)[s:i])
-			s = i + 1
-			if len(seg) == 0 {
+			if b.Len() == 0 && s == i {
+				s = i + 1
 				continue
-			} else {
-				if !xlab && lbuf.Len() > 0 {
-					label = lbuf.String()[1:]
+			}
+			b.Write((*path)[s:i])
+			segment = b.String()
+			b.Reset()
+			if wb.Len() > 0 {
+				wildcard = wb.String()
+				if c == 1 {
+					wildcard = wildcard[1:]
 				}
-				*path = (*path)[s:]
-				return
+				wb.Reset()
 			}
-		case '[':
-			xlab = true
-		case ']':
-			xlab = false
-		case '*':
-			if !xlab {
-				w = true
-			}
+			bufPool.Put(b)
+			bufPool.Put(wb)
+			*path = (*path)[i+1:]
+			return
 		}
-		if xlab {
-			lbuf.WriteByte((*path)[i])
+		if bs >= s && be > bs {
+			wb.WriteByte(';')
+			wb.Write((*path)[bs+1 : be])
+			b.Write((*path)[s:bs])
+			b.WriteByte('*')
+			c++
+			s = be + 1
+			bs, be = -1, -1
+			fuzzy = true
 		}
 	}
-	seg = string((*path)[s:])
-	if !xlab && lbuf.Len() > 0 {
-		label = lbuf.String()[1:]
+	b.Write((*path)[s:])
+	segment = b.String()
+	b.Reset()
+	if wb.Len() > 0 {
+		wildcard = wb.String()
+		if c == 1 {
+			wildcard = wildcard[1:]
+		}
+		wb.Reset()
 	}
+	bufPool.Put(b)
+	bufPool.Put(wb)
 	*path = (*path)[len(*path):]
 	return
 }
 
-func (n *Node) Insert(path []byte, value []uint64, labels map[string]string) (err error) {
+func (n *Node) Insert(key string, path []byte, userIDs []uint32) (err error) {
+	return n.insert(key, path, userIDs, nil, 0)
+}
+
+func (n *Node) insert(
+	key string,
+	path []byte,
+	userIDs []uint32,
+	// segment index => [0 -> wildcard 1 -> key]
+	wildcards map[int][2]string,
+	cur int,
+) (
+	err error,
+) {
 RESTART:
 	if len(path) == 0 {
-		if len(value) > 0 {
-			if n.Container == nil {
-				n.Container = roaring64.New()
+		if len(userIDs) > 0 {
+			sort.Slice(userIDs, func(i, j int) bool {
+				return userIDs[i] < userIDs[j]
+			})
+			if n.Leaf {
+				if n.Container != nil {
+					if userIDs[0] == 0 {
+						n.Container = nil
+					} else {
+						n.Container.AddMany(userIDs)
+					}
+				}
+			} else {
+				n.Leaf = true
+				if userIDs[0] != 0 {
+					n.Container = roaring.New()
+					n.Container.AddMany(userIDs)
+				}
 			}
-			n.Container.AddMany(value)
+			if len(wildcards) > 0 {
+				if n.Wildcards == nil {
+					n.Wildcards = make(map[int]map[string]string)
+				}
+				for i, val := range wildcards {
+					if _, ok := n.Wildcards[i]; !ok {
+						n.Wildcards[i] = make(map[string]string)
+					}
+					n.Wildcards[i][val[0]] = val[1]
+				}
+			}
 		}
-		n.Labels = labels
-		n.End = true
 		return
 	}
-	seg, label, wildcard := n.ipop(&path)
-	if len(label) > 0 {
-		seg = seg[len(label)+2:]
-		if labels == nil {
-			labels = make(map[string]string)
+	segment, wildcard, fuzzy := n.ipop(&path)
+	if len(segment) == 0 {
+		goto RESTART
+	} else if len(wildcard) > 0 {
+		if wildcards == nil {
+			wildcards = make(map[int][2]string)
 		}
-		labels[label] = seg
-		if len(seg) == 0 {
-			goto RESTART
-		}
+		wildcards[cur] = [2]string{wildcard, key}
 	}
-	if wildcard && (len(seg) > 1 || seg[0] != '*') {
-		buf := &bytes.Buffer{}
-		buf.WriteByte('^')
-		buf.WriteString(strings.ReplaceAll(seg, "*", "(.+?)"))
-		buf.WriteByte('$')
-		seg = buf.String()
+	if fuzzy && (len(segment) > 1 || segment[0] != '*') {
+		b := bufPool.New().(*bytes.Buffer)
+		b.WriteByte('^')
+		b.WriteString(strings.ReplaceAll(segment, "*", "(.+?)"))
+		b.WriteByte('$')
+		segment = b.String()
 		if n.Regexps == nil {
 			n.Regexps = make(map[string]*regexp.Regexp)
 		}
-		if _, ok := n.Regexps[seg]; !ok {
+		if _, ok := n.Regexps[segment]; !ok {
 			var reg *regexp.Regexp
-			if reg, err = regexp.Compile(seg); err != nil {
+			if reg, err = regexp.Compile(segment); err != nil {
 				return
 			}
-			n.Regexps[seg] = reg
+			n.Regexps[segment] = reg
 		}
 	}
 	if n.Childrens == nil {
 		n.Childrens = make(map[string]*Node)
 	}
-	if child, x := n.Childrens[seg]; x {
-		return child.Insert(path, value, labels)
+	if child, x := n.Childrens[segment]; x {
+		return child.insert(key, path, userIDs, wildcards, cur+1)
 	}
-	n.Childrens[seg] = &Node{}
-	return n.Childrens[seg].Insert(path, value, labels)
+	n.Childrens[segment] = &Node{}
+	return n.Childrens[segment].insert(key, path, userIDs, wildcards, cur+1)
 }
 
-func (n *Node) mpop(path *[]byte) (seg string) {
+func (n *Node) mpop(path *[]byte) (segment string) {
 	if len(*path) == 0 {
 		return
 	}
@@ -124,36 +196,78 @@ func (n *Node) mpop(path *[]byte) (seg string) {
 			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z':
 			(*path)[i] += 32 // lower case
 		case '/':
-			seg = string((*path)[s:i])
-			s = i + 1
-			if seg == "" {
+			if s == i {
+				s = i + 1
 				continue
-			} else {
-				*path = (*path)[s:]
-				return
 			}
+			segment = string((*path)[s:i])
+			*path = (*path)[i+1:]
+			return
 		}
 	}
-	seg = string((*path)[s:])
+	segment = string((*path)[s:])
 	*path = (*path)[len(*path):]
 	return
 }
 
-func (n *Node) Match(path []byte, value uint64) (has, ok bool, labels map[string]string) {
+func (n *Node) Match(path []byte, userID uint32) (has, ok bool, params map[string]string) {
+	var wildcardElements []map[string][2]string
+	has, ok, wildcardElements = n.match(path, userID, 0, make(map[int]string))
+	if len(wildcardElements) > 0 {
+		params = make(map[string]string)
+		for _, val := range wildcardElements {
+			for wildcard, v := range val {
+				if wildcard[0] == ';' {
+					keys := strings.Split(wildcard, ";")
+					vals := strings.Split(v[0], ";")
+					for i := 1; i < len(keys); i++ {
+						params[keys[i]] = vals[i]
+					}
+				} else {
+					params[wildcard] = v[0]
+				}
+			}
+		}
+	}
+	return
+}
+
+func (n *Node) match(
+	path []byte,
+	userID uint32,
+	cur int,
+	segmentWildcardElements map[int]string,
+) (
+	has, ok bool,
+	// wildcard key => [0 -> element 1 -> key]
+	wildcardElements []map[string][2]string,
+) {
 	if len(path) == 0 {
-		if !n.End {
+		if !n.Leaf {
 			return
 		}
 		has = true
-		labels = n.Labels
-		if value == 0 {
+		if len(n.Wildcards) > 0 {
+			for cur, val := range n.Wildcards {
+				tmp := make(map[string][2]string)
+				for wildcard, key := range val {
+					tmp[wildcard] = [2]string{segmentWildcardElements[cur], key}
+				}
+				wildcardElements = append(wildcardElements, tmp)
+			}
+		}
+		if userID == 0 {
 			ok = n.Container == nil
-		} else if n.Container != nil {
-			ok = n.Container.Contains(value)
+		} else {
+			if n.Container != nil {
+				ok = n.Container.Contains(userID)
+			} else {
+				ok = true
+			}
 		}
 		return
 	}
-	seg := n.mpop(&path)
+	segment := n.mpop(&path)
 
 	//     I have done an optimization experiment that using goroutine, and the results
 	// show that although the overall average time-consuming is much more stable, the
@@ -167,31 +281,34 @@ func (n *Node) Match(path []byte, value uint64) (has, ok bool, labels map[string
 
 	// type1: regexp
 	for pattern, reg := range n.Regexps {
-		if reg.MatchString(seg) {
-			if _has, _ok, _labels := n.Childrens[pattern].Match(path, value); _ok {
-				has, ok, labels = _has, _ok, _labels
+		if matches := reg.FindStringSubmatch(segment); matches != nil {
+			matches[0] = ""
+			segmentWildcardElements[cur] = strings.Join(matches, ";")
+			if _has, _ok, _wildcardElements := n.Childrens[pattern].match(path, userID, cur+1, segmentWildcardElements); _ok {
+				has, ok, wildcardElements = _has, _ok, _wildcardElements
 				return
 			} else if _has {
-				has, labels = _has, _labels
+				has, wildcardElements = _has, _wildcardElements
 			}
 		}
 	}
 	// type2: key
-	if child, x := n.Childrens[seg]; x {
-		if _has, _ok, _labels := child.Match(path, value); _ok {
-			has, ok, labels = _has, _ok, _labels
+	if child, x := n.Childrens[segment]; x {
+		if _has, _ok, _wildcardElements := child.match(path, userID, cur+1, segmentWildcardElements); _ok {
+			has, ok, wildcardElements = _has, _ok, _wildcardElements
 			return
 		} else if _has {
-			has, labels = _has, _labels
+			has, wildcardElements = _has, _wildcardElements
 		}
 	}
-	// type3: wildcard
+	// type3: all
 	if child, x := n.Childrens["*"]; x {
-		if _has, _ok, _labels := child.Match(path, value); _ok {
-			has, ok, labels = _has, _ok, _labels
+		segmentWildcardElements[cur] = segment
+		if _has, _ok, _wildcardElements := child.match(path, userID, cur+1, segmentWildcardElements); _ok {
+			has, ok, wildcardElements = _has, _ok, _wildcardElements
 			return
 		} else if _has {
-			has, labels = _has, _labels
+			has, wildcardElements = _has, _wildcardElements
 		}
 	}
 	return
@@ -202,11 +319,11 @@ func (n *Node) Dump(text *string, part ...string) {
 	if len(part) > 0 {
 		p = part[0]
 	}
-	if n.End {
+	if n.Leaf {
 		if n.Container == nil {
-			*text += fmt.Sprintf("%s %v -\n", p, n.Labels)
+			*text += fmt.Sprintf("%s %v -\n", p, n.Wildcards)
 		} else {
-			*text += fmt.Sprintf("%s %v %v\n", p, n.Labels, n.Container.ToArray())
+			*text += fmt.Sprintf("%s %v %v\n", p, n.Wildcards, n.Container.ToArray())
 		}
 	}
 	for s, node := range n.Childrens {
